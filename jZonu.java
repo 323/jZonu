42b776b4c4e6c83c3d7fb1f9b4941e6d9845cda2
323 <starcraft47@gmail.com>
2014-01-08 15:41:37 -0500

    Initial Commit
    No functionality yet, just putting it up on GitHub.

---
+++ jZonu.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+import java.io.UnsupportedEncodingException; 
+import org.apache.commons.lang3.StringUtils;
+
+class jZonu {
+    public static void main(String[] args) {
+        //jZonu - A Java re-write of Zonu
+        //Coded by tritutri
+        
+        HTTPRequest request = new HTTPRequest();
+        request.setURL("http://www.secretareaofvipquality.net/saovq/");
+    }
+}
