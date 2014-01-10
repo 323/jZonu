92334c5937e959d8af11135cdd86a902cefa8859
323 <starcraft47@gmail.com>
2014-01-09 22:07:17 -0500

    Finished a lot of SQL stuff
    Finished a lot of new SQL stuff, as well as testing sending scrape
    results to the DB

--- jZonu.java
+++ jZonu.java
@@ -5,9 +5,13 @@ import org.apache.commons.lang3.StringUtils;
 import java.sql.*;
 
 class jZonu {
-    public static void main(String[] args) {
-        //jZonu - A Java re-write of Zonu
-        //Coded by 323/tritutri
+    //jZonu - A Java re-write of Zonu
+    //Coded by 323/tritutri
+    Connection connect = null;
+    Statement statement = null;
+    PreparedStatement preparedStatement = null;
+    ResultSet resultSet = null;
+    public void main(String[] args) throws Exception {
         HTTPRequest request = new HTTPRequest();
         String searchstring1, searchstring2;
         String[] frontPageSources = new String[15]; //14 because 15 posts
@@ -27,6 +31,73 @@ class jZonu {
             frontPageContent[i][5] = StringUtils.substringBetween(frontPageSources[i], "</span> : ", "  <span class=\"delete"); //get post date
             frontPageContent[i][6] = StringUtils.substringBetween(frontPageSources[i], "replytext\">", "</div> </div>"); //get post content
         }
-        
+        try {
+            Class.forName("com.mysql.jdbc.Driver"); //load jdbc driver
+            connect = DriverManager.getConnection("jdbc:mysql://localhost/feedback?" + "user=root2&password=root2"); //connect to sql db
+            statement = connect.createStatement(); //allow issuing sql queries to db
+            preparedStatement = connect.prepareStatement("insert into CHANARCHIVES.SAOVQ values (?, ?, ?, ?, ?, ?, ?)");
+            //later, put the whole SQL part into a loop so it saves all of the content
+            preparedStatement.setString(0, frontPageContent[0][0]);
+            preparedStatement.setString(1, frontPageContent[0][1]);
+            preparedStatement.setString(2, frontPageContent[0][2]);
+            preparedStatement.setString(3, frontPageContent[0][3]);
+            preparedStatement.setString(4, frontPageContent[0][4]);
+            preparedStatement.setString(5, frontPageContent[0][5]);
+            preparedStatement.setString(6, frontPageContent[0][6]);
+            
+            preparedStatement = connect.prepareStatement("SELECT ThreadNumber, ThreadName, NumOfReplies, PosterName, PosterTripcode, PostDate, PostContent from CHANARCHIVES.SAOVQ");
+            resultSet = preparedStatement.executeQuery();
+            writeResultSet(resultSet);
+            
+            resultSet = statement.executeQuery("select * from CHANARCHIVES.SAOVQ");
+            writeMetaData(resultSet);
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            close();
+        }
+    }
+    //Just some testing stuff
+    private void writeMetaData(ResultSet resultSet) throws SQLException {
+        System.out.println("The columns in the table are: ");
+        System.out.println("Table : " + resultSet.getMetaData().getTableName(1));
+        for (int i=1; i<=resultSet.getMetaData().getColumnCount(); i++){
+            System.out.println("Column " + i + " " + resultSet.getMetaData().getColumnName(i));
+        }
+    }
+    //More testing stuff
+    private void writeResultSet(ResultSet resultSet) throws SQLException {
+        while (resultSet.next()) {
+            String threadnumber = resultSet.getString("ThreadNumber");
+            String threadname = resultSet.getString("ThreadName");
+            String numofreplies = resultSet.getString("NumOfReplies");
+            String postername = resultSet.getString("PosterName");
+            String postertrip = resultSet.getString("PosterTripcode");
+            String postdate = resultSet.getString("PostDate");
+            String postcontent = resultSet.getString("PostContent");
+            System.out.println("Thread Num: " + threadnumber);
+            System.out.println("Thread Name: " + threadname);
+            System.out.println("Number of Replies: " + numofreplies);
+            System.out.println("Poster Name: " + postername);
+            System.out.println("Poster Trip: " + postertrip);
+            System.out.println("Post Date: " + postdate);
+            System.out.println("Post Content: " + postcontent);
+        }
+    }
+    //to close resultSet
+    private void close() {
+         try {
+             if (resultSet != null) {
+                 resultSet.close();
+             }
+             if (statement != null) {
+                 statement.close();
+             }
+             if (connect != null) {
+                 connect.close();
+             }
+        } catch (Exception e) {
+            
+        }
     }
 }
